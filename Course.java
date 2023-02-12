0084c3dfee6ac8732d143d987bb936069c11674e
Alena Smolenchuk <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-02-12 16:48:36 +0300

    Create Course.java

---
+++ Course.java
@@ -0,0 +1,51 @@
+public class Course {
+
+    public String code;
+    public String currencyName;
+    public String amount;
+    public String course;
+    public String change;
+
+    public Course() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getcurrencyName() {
+        return currencyName;
+    }
+
+    public void setcurrencyName(String currencyName) {
+        this.currencyName = currencyName;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getChange() {
+        return change;
+    }
+
+    public void setChange(String change) {
+        this.change = change;
+    }
+}
