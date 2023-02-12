4a23de8b5c8525cf7a61295a1f596574c102b185
Alena Smolenchuk <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-02-12 16:49:15 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+import org.apache.log4j.BasicConfigurator;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+
+public class Main {
+
+    public static void main(String[] arg)  {
+        BasicConfigurator.configure();
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new ConfigBot());
+        } catch (Exception e) {
+        }
+    }
+}
