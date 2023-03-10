0560333f85dbeaf62f218cdf92fcabf3f8d709bc
Alena Smolenchuk <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-03-10 19:25:03 +0300

    Update ConfigBot.java

--- src/java/ConfigBot.java
+++ src/java/ConfigBot.java
@@ -1,3 +1,4 @@
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
