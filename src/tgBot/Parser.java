f69dae5b0d798c2023ba02ee517ae8ffd20a360a
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-05-10 20:42:45 +0300

    Update Parser.java

--- src/tgBot/Parser.java
+++ src/tgBot/Parser.java
@@ -2,7 +2,7 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Element; 
 
 import java.io.IOException;
 import java.net.URL;
