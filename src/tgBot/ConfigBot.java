63c27f8d8ab5dc8615929cbaaf19c95a06d51bae
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-06-08 04:41:54 +0300

    Update ConfigBot.java

--- src/tgBot/ConfigBot.java
+++ src/tgBot/ConfigBot.java
@@ -2,7 +2,7 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton; 
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;  
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException; 
  
 import java.io.IOException;
